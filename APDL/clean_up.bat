ab6ce153b1c2ba699146ff0eca7f1ff3ccd2a2cf
Armando Bringas <armando.bringas@gmail.com>
2019-10-20 16:39:20 -0500

    Added "clean_up.bat"
    Added "clean_up.bat" file to delete all ANSYS APDL session files. Clean-up analysis folder

---
+++ APDL/clean_up.bat
@@ -0,0 +1,17 @@
+@echo off
+
+del /S /Q /F *.DSP
+del /S /Q /F *.emat
+del /S /Q /F *.err
+del /S /Q /F *.esav
+del /S /Q /F *.full
+del /S /Q /F *.lock
+del /S /Q /F *.log
+del /S /Q /F *.mlv
+del /S /Q /F *.mntr
+del /S /Q /F *.mode
+del /S /Q /F *.rst
+del /S /Q /F *.stat
+del /S /Q /F *.out
+del /S /Q /F *.page
+del /S /Q /F *.tmp
